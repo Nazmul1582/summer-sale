0987651de6e19963b1e155bd0e852267f7dbcf88
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-08-20 16:03:49 +0600

    get all selected products price and set them to the total price field

--- js/sale.js
+++ js/sale.js
@@ -12,6 +12,10 @@ const chair = document.getElementById("single-relax-chair");
 const childrenPlay = document.getElementById("children-play");
 const sofa = document.getElementById("sofa");
 
+const totalPriceField = document.getElementById("total-price");
+const discountField = document.getElementById("discount");
+const totalField = document.getElementById("total");
+
 // get product name
 function getProductName(targetedElement) {
   let card = targetedElement.parentNode;
@@ -26,16 +30,36 @@ function getProductPrice(targetedElement) {
   return price;
 }
 
-// set product in product list
+const totalPriceArr = [];
+
+// get all selected product
 function setProduct(targetedElement) {
   targetedElement.addEventListener("click", function () {
     let productName = getProductName(targetedElement);
     let li = document.createElement("li");
     li.innerText = productName;
     productList.appendChild(li);
+
+    // get selected product's price and set total price
+    let priceString = getProductPrice(targetedElement);
+    let price = priceString.slice(0, -3);
+    let actualPrice = parseInt(price);
+    totalPriceArr.push(actualPrice);
+
+    setTotalPrice();
   });
 }
 
+// calculate total price
+function setTotalPrice() {
+  let sum = 0;
+  for (p of totalPriceArr) {
+    sum += p;
+  }
+  totalPriceField.innerText = sum.toFixed(2);
+}
+
+// select products for puschase
 setProduct(kAccessories);
 setProduct(accessories);
 setProduct(cooker);
@@ -45,9 +69,3 @@ setProduct(sportsCates);
 setProduct(chair);
 setProduct(childrenPlay);
 setProduct(sofa);
-
-// kAccessories.addEventListener("click", function () {
-//   const list = document.createElement("li");
-//   list.innerText = "k.accessories";
-//   productList.appendChild(list);
-// });
